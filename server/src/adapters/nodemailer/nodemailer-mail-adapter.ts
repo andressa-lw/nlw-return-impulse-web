b1979e7d66537ead5d80609e215ebb311c1cd913
Andressa Bendette <andressa@lumosweb.com.br>
2022-05-06 11:25:26 -0300

    first commit

---
+++ server/src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import { MailAdapter, SendMailData } from "../mail-adapter";
+import nodemailer from 'nodemailer';
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "05a08c7de3d2bf",
+    pass: "5bf98d1413c708"
+  }
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: 'Equipe Feedget <oi@feedget.com>',
+      to: 'Andressa Benedette <andressa@lumosweb.com.br>',
+      subject,
+      html: body,
+    });
+  };
+}
